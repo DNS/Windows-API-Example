9dc4db187f91ff062acd0733029b8c624b8fdfe9
SiraitX <SiraitX@localhost>
2014-01-14 00:36:17 +0700

    M: Bug Fix

--- simplewindow.c
+++ simplewindow.c
@@ -26,12 +26,6 @@
 #define IDM_FILE_CONTROL 7
 
 
-#define ID_BLUE 6001
-#define ID_YELLOW 6002
-#define ID_ORANGE 6003
-
-WCHAR buf[500];
-
 HFONT defaultFont;
 HWND ghSb;
 HMENU hMenubar;
@@ -39,17 +33,19 @@ HMENU hMenu;
 HINSTANCE ghInstance;
 HWND ghwndEdit;
 HWND hEdit , hLabel, button1, button2, checkbox1;
-HFONT hFont, hFont_default;
+HWND radiobtn1, radiobtn2, radiobtn3;
+HFONT hfont1, hfont2, hfont3;
 HBITMAP hBitmap;
 NONCLIENTMETRICS ncm;
 HWND hTrack;
 UINT hTrack_id;
 HWND hDebugLabel;
-HWND hMonthCal;
-COLORREF g_color;
+HWND hMonthCal, hCombo, groupbox1;
+HWND hDlgCurrent = NULL;
 
 LRESULT CALLBACK WndProc (HWND, UINT, WPARAM, LPARAM);
 LRESULT CALLBACK DialogProc (HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK ControlProc (HWND, UINT, WPARAM, LPARAM);
 LRESULT CALLBACK PanelProc (HWND, UINT, WPARAM, LPARAM);
 
 void CreateDialogBox (HWND);
@@ -64,8 +60,10 @@ void CreateTrackBar(HWND);
 void UpdateTrackBar();
 void AddMenus (HWND);
 
-
 HMODULE hmod;
+
+
+WCHAR buf[500];
 
 
 
@@ -104,28 +102,39 @@ int WINAPI wWinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLin
 	//InitCommonControls();		// obsolete
 	InitCommonControlsEx(&iccex);
 
-	hwnd = CreateWindowExW(WS_EX_WINDOWEDGE | WS_EX_ACCEPTFILES , wc.lpszClassName, L"Title", 
-		WS_VISIBLE | WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN, CW_USEDEFAULT, 
+	hwnd = CreateWindowExW(WS_EX_WINDOWEDGE | WS_EX_ACCEPTFILES | WS_EX_CONTROLPARENT, wc.lpszClassName, L"Title", 
+		WS_VISIBLE | WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_TABSTOP, CW_USEDEFAULT, 
 		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
-
+	
+	
 	//ShowWindow(hwnd, nCmdShow);
 	//UpdateWindow(hwnd);
 
 	while (GetMessageW(&msg, NULL, 0, 0) > 0) { /* If no error is received... */
-		TranslateMessage(&msg);		/* Translate key codes to chars if present */
-		DispatchMessageW(&msg);		/* Send it to WndProc */
+		if (!IsDialogMessageW(hDlgCurrent, &msg)) {
+			TranslateMessage(&msg);		/* Translate key codes to chars if present */
+			DispatchMessageW(&msg);		/* Send it to WndProc */
+		}
 	}
 
 	return (int) msg.wParam;
 }
 
 
+BOOL CALLBACK EnumFunc (HWND hwnd, LPARAM lParam) {
+	DestroyWindow(hwnd);
+	//SendMessageW(hwnd_tmp, WM_DESTROY, 0, 0);
+	//MessageBoxW(NULL, L"First Program", L"First", MB_OK);
+}
+
 LRESULT CALLBACK WndProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	HWND hwnd_tmp;
+	MSG msg_tmp;
 	POINT point;
 	UINT state;
 	static HWND hwndPanel;
-	HWND hwnd_tmp;
 	RECT rectParent;
+	WNDCLASSEX wc = {0};
 
 	switch (msg) {
 		case WM_CREATE:
@@ -140,26 +149,30 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 				0, 0, 0, 0, hwnd, (HMENU) 5003, NULL, NULL );
 			ShowWindow(ghSb, SW_HIDE);
 
-			RegisterDialogClass(hwnd);
+			
 			CreateMyTooltip(hwnd);
 			
-			// Delphi: Tahoma 13, .NET: Microsoft Sans Serif 14, System Default: Segoe UI
-			
+			// Delphi: Tahoma 13, .NET: Microsoft Sans Serif 14, System Default: Segoe UI 15
 			// get custom font
-			hFont = CreateFontW(15, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, 
+			hfont1 = CreateFontW(13, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, 
+				OUT_TT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, 
+				DEFAULT_PITCH | FF_DONTCARE, L"Tahoma");
+
+			hfont3 = CreateFontW(15, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, 
 				OUT_TT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, 
 				DEFAULT_PITCH | FF_DONTCARE, L"Comic Sans MS");
 
-			// get system default font
+			// get system default font (default: Segoe UI 15)
 			ncm.cbSize = sizeof(NONCLIENTMETRICS);
 			SystemParametersInfo(SPI_GETNONCLIENTMETRICS, sizeof(NONCLIENTMETRICS), &ncm, 0);
-			hFont_default = CreateFontIndirect(&ncm.lfMessageFont);
+
+			hfont2 = CreateFontIndirect(&ncm.lfMessageFont);
 			
 			ghwndEdit = CreateWindowW(L"EDIT", L"abcd",
 				WS_VISIBLE | WS_CHILD | WS_HSCROLL | WS_VSCROLL | ES_MULTILINE | ES_WANTRETURN | WS_CLIPCHILDREN,
 				0, 0, 260, 180, hwnd, NULL, NULL, NULL);
 				
-			//SendMessageW(ghwndEdit, WM_SETFONT, (WPARAM) hFont, TRUE);
+			//SendMessageW(ghwndEdit, WM_SETFONT, (WPARAM) hfont1, TRUE);
 
 			/* // RICH EDIT CONTROL
 			LoadLibrary(TEXT("Msftedit.dll"));
@@ -185,78 +198,44 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 					Beep(50, 100);
 					break;
 				case IDM_FILE_DIALOG:
-					CreateDialogBox(hwnd);
+					memset(&wc, 0, sizeof(wc));
+					wc.cbSize = sizeof(WNDCLASSEX);
+					wc.lpfnWndProc = (WNDPROC) DialogProc;
+					wc.hInstance = GetModuleHandle(NULL);
+					wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
+					wc.lpszClassName = L"DialogClass";
+					RegisterClassExW(&wc);
+					CreateWindowExW(WS_EX_DLGMODALFRAME | WS_EX_TOPMOST, TEXT("DialogClass"), 
+						TEXT("Dialog Box"), WS_VISIBLE | WS_SYSMENU | WS_CAPTION, 100, 100, 200, 150, 
+						hwnd, (HMENU) NULL, GetModuleHandle(NULL),NULL);
 					break;
 				case IDM_FILE_COLOR:
 					gColor = ShowColorDialog(hwnd);
 					InvalidateRect(hwndPanel, NULL, TRUE);
 					break;
 				case IDM_FILE_CONTROL:
-					hwnd_tmp = CreateWindowExW(WS_EX_DLGMODALFRAME | WS_EX_TOPMOST, L"DialogClass", 
+					//RegisterDialogClass(hwnd);
+					memset(&wc, 0, sizeof(wc));
+					wc.cbSize = sizeof(WNDCLASSEX);
+					wc.lpfnWndProc = (WNDPROC) ControlProc;
+					wc.hInstance = ghInstance;
+					wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
+					wc.lpszClassName = L"ControlClass";
+					RegisterClassExW(&wc);
+					
+					hwnd_tmp = CreateWindowExW(WS_EX_DLGMODALFRAME | WS_EX_CONTROLPARENT, L"ControlClass", 
 						L"Dialog Box", WS_VISIBLE | WS_SYSMENU | WS_CAPTION, 
-						100, 100, 800, 550, hwnd, NULL, GetModuleHandle(NULL), NULL);
-					
-					// default Height: Edit 21, Button 25, Static 13 
-					
-					hLabel = CreateWindowW(L"STATIC", L"This is Label", 
-						WS_CHILD | WS_VISIBLE | SS_LEFT, 
-						20, 90, 300, 13, hwnd_tmp, (HMENU) 500, NULL, NULL);
-					button1 = CreateWindowW(L"BUTTON", L"Button", WS_VISIBLE | WS_CHILD, 
-						20, 50, 80, 25, hwnd_tmp, (HMENU) 600, NULL, NULL);
-					button2 = CreateWindowW(L"BUTTON", L"&Quit", WS_VISIBLE | WS_CHILD, 
-						120, 50, 80, 25, hwnd_tmp, (HMENU) 650, NULL, NULL);
-					checkbox1 = CreateWindowW(L"BUTTON", L"This is Checkbox", 
-						WS_VISIBLE | WS_CHILD | BS_CHECKBOX, 
-						20, 10, 185, 14, hwnd_tmp, (HMENU) 700, NULL, NULL);
+						100, 100, 800, 550, hwnd, (HMENU) NULL, GetModuleHandle(NULL), NULL);
 
-					hDebugLabel = CreateWindowW(L"STATIC", L"0", WS_CHILD | WS_VISIBLE, 
-						170, 20, 90, 30, hwnd_tmp, (HMENU) 3004, NULL, NULL);
+					EnableWindow(hwnd, FALSE);	// make hwnd_tmp modal window
 
-					// EDIT ctrl: max 32,767 bytes
-					hEdit = CreateWindowExW(WS_EX_CLIENTEDGE, L"EDIT", L"This is edit", 
-						WS_VISIBLE | WS_CHILD | ES_LEFT | ES_AUTOHSCROLL | ES_NOHIDESEL,
-						20, 110, 185, 21, hwnd_tmp, (HMENU) 800, NULL, NULL);	// WS_BORDER
 
-					CheckDlgButton(hwnd_tmp, 1, BST_CHECKED);
-
-					// better font
-					SendMessageW(hEdit, WM_SETFONT, (WPARAM) hFont, TRUE);
-					SendMessageW(hLabel, WM_SETFONT, (WPARAM) hFont, TRUE);
-					//SendMessageW(button1, WM_SETFONT, (WPARAM) hFont, TRUE);
-					SendMessageW(button2, WM_SETFONT, (WPARAM) hFont, TRUE);
-					SendMessageW(checkbox1, WM_SETFONT, (WPARAM) hFont, TRUE);
-					
-					SendMessageW(hEdit, WM_SETFONT, (WPARAM) hFont_default, MAKELPARAM(TRUE, 0));
-					SendMessageW(hLabel, WM_SETFONT, (WPARAM) hFont_default, MAKELPARAM(TRUE, 0));
-					
-					//ShowWindow(hwnd_tmp, SW_SHOW);	// already WS_VISIBLE
-
-					CreateTrackBar(hwnd_tmp);
-					// SysMonthCal32: 1. Standard 178, 156 + WS_BORDER; 2. Luna 230, 170;
-					hMonthCal = CreateWindowW(L"SysMonthCal32", L"",          
-						WS_VISIBLE | WS_CHILD,  
-						20, 200, 225, 160, hwnd_tmp, (HMENU) 4001, NULL, NULL);
-
-					// GroupBox
-					CreateWindowW(TEXT("button"), TEXT("Choose Color"), 
-							WS_CHILD | WS_VISIBLE | BS_GROUPBOX,
-							260, 20, 120, 150, hwnd_tmp, (HMENU) NULL, NULL, NULL);
-					CreateWindowW(TEXT("button"), TEXT("Blue"),
-							WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON,
-							270, 40, 100, 30, hwnd_tmp, (HMENU) ID_BLUE, NULL, NULL);
-					CreateWindowW(TEXT("button"), TEXT("Yellow"),
-							WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON,
-							270, 65, 100, 30, hwnd_tmp, (HMENU) ID_YELLOW, NULL, NULL);
-					CreateWindowW(TEXT("button"), TEXT("Orange"),
-							WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON,
-							270, 90, 100, 30, hwnd_tmp, (HMENU) ID_ORANGE, NULL, NULL);
-					
 					break;
 				case IDM_FILE_OPEN:
 					OpenDialog(hwnd);
 					break;
 				case IDM_FILE_QUIT:
-					SendMessage(hwnd, WM_CLOSE, 0, 0);
+					SendMessageW(hwnd, WM_CLOSE, 0, 0);
 					break;
 				case IDM_VIEW_STB:
 					state = GetMenuState(hMenu, IDM_VIEW_STB, MF_BYCOMMAND);
@@ -279,16 +258,13 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 			break;
 		case WM_SIZING:
 			GetClientRect(hwnd, &rectParent);
-			//MoveWindow(ghwndEdit, rectParent.top, rectParent.left, rectParent.right, rectParent.bottom, 
-			//	FALSE);
-			SetWindowPos(ghwndEdit, 0, rectParent.top, rectParent.left, rectParent.right, rectParent.bottom, 0);
+			MoveWindow(ghwndEdit, rectParent.top, rectParent.left, rectParent.right, rectParent.bottom, FALSE);
+			// MoveWindow and SetWindowPos are similar but SetWindowsPos provides Z-ordering of the window
+			//SetWindowPos(ghwndEdit, 0, rectParent.top, rectParent.left, rectParent.right, rectParent.bottom, 0);
 			break;
 		case WM_EXITSIZEMOVE:
-			//GetClientRect(hwnd, &rectParent);
-			//MoveWindow(ghwndEdit, rectParent.top, rectParent.left, rectParent.right, rectParent.bottom, TRUE); 
 			break;
 		case WM_SETFOCUS:
-			//SetFocus(GetDlgItem(hwnd, 0));
 			break;
 		case WM_RBUTTONUP:
 			point.x = LOWORD(lParam);
@@ -307,13 +283,18 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 		case WM_LBUTTONDOWN:
 			break;
 		case WM_CLOSE:
+			//EnumWindows(EnumFunc, lParam);
 			DestroyWindow(hwnd);
 			break;
+		case WM_SYSCOMMAND:
+			break;
 		case WM_DESTROY:
-			DeleteObject(hFont);
-			DeleteObject(hFont_default);
+			DeleteObject(hfont1);
+			DeleteObject(hfont2);
+			DeleteObject(hfont3);
 			DeleteObject(hBitmap);
 			FreeLibrary(hmod);
+
 			PostQuitMessage(0);
 			break;
 		return 0;
@@ -350,20 +331,101 @@ void AddMenus (HWND hwnd) {
 }
 
 
-LRESULT CALLBACK DialogProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK ControlProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	HWND hwnd_parent;
 	UINT checked;
 	int ctrlID;
 	int requestID;
 	int position;
 	LPNMHDR lpNmHdr;
 	SYSTEMTIME time;
+	int sel;
+	BOOL ret;
+	WCHAR os_list[5][32] = {L"Windows 98 SE", L"Windows ME", L"Windows XP", L"Windows 7", L"Windows Phone 8"};
 
+	//IsDialogMessageW(hwnd, (LPMSG) &msg);
 	switch (msg) {
 		case WM_CREATE:
-			/*CreateWindow(TEXT("button"), TEXT("Ok"),
-			WS_VISIBLE | WS_CHILD ,
-			50, 50, 80, 25,
-			hwnd, (HMENU) 2, NULL, NULL);*/
+			// default Height: Edit 21, Button 25, Static 13, CheckBox 17 
+			hLabel = CreateWindowW(L"STATIC", L"This is Label", 
+				WS_CHILD | WS_VISIBLE | SS_LEFT | WS_TABSTOP, 
+				20, 80, 150, 13, hwnd, (HMENU) 500, NULL, NULL);
+			button1 = CreateWindowW(L"BUTTON", L"&Button", WS_VISIBLE | WS_CHILD | WS_TABSTOP,
+				20, 50, 80, 25, hwnd, (HMENU) 600, NULL, NULL);
+			button2 = CreateWindowW(L"BUTTON", L"&Quit", WS_VISIBLE | WS_CHILD | WS_TABSTOP | WS_DISABLED,
+				120, 50, 80, 25, hwnd, (HMENU) 650, NULL, NULL);
+			
+			checkbox1 = CreateWindowW(L"BUTTON", L"This is &Checkbox", 
+				WS_VISIBLE | WS_CHILD | BS_CHECKBOX | WS_TABSTOP, 
+				20, 10, 110, 17, hwnd, (HMENU) 700, NULL, NULL);
+			// BST_CHECKED / BST_INDETERMINATE / BST_UNCHECKED
+			//CheckDlgButton(hwnd, 700, BST_CHECKED);	// same below
+			SendMessageW(checkbox1, BM_SETCHECK, BST_CHECKED, 0);
+
+			hDebugLabel = CreateWindowW(L"STATIC", L"Debug Label", WS_CHILD | WS_VISIBLE, 
+				550, 20, 200, 13, hwnd, (HMENU) 3111, NULL, NULL);
+
+			// EDIT ctrl: max 32,767 bytes
+			hEdit = CreateWindowExW(WS_EX_CLIENTEDGE, L"EDIT", L"This is edit", 
+				WS_VISIBLE | WS_CHILD | ES_LEFT | ES_AUTOHSCROLL | ES_NOHIDESEL,
+				20, 110, 185, 21, hwnd, (HMENU) 800, NULL, NULL);	// WS_BORDER
+
+			
+
+			// better font
+			SendMessageW(hEdit, WM_SETFONT, (WPARAM) hfont1, TRUE);
+			SendMessageW(hLabel, WM_SETFONT, (WPARAM) hfont1, TRUE);
+			SendMessageW(button2, WM_SETFONT, (WPARAM) hfont1, TRUE);
+			SendMessageW(checkbox1, WM_SETFONT, (WPARAM) hfont3, TRUE);
+
+			SendMessageW(button1, WM_SETFONT, (WPARAM) hfont2, TRUE);	
+			SendMessageW(hEdit, WM_SETFONT, (WPARAM) hfont2, MAKELPARAM(TRUE, 0));
+			SendMessageW(hDebugLabel, WM_SETFONT, (WPARAM) hfont1, TRUE);
+			
+			//ShowWindow(hwnd_tmp, SW_SHOW);	// already WS_VISIBLE
+
+			CreateTrackBar(hwnd);
+
+			// Calendar
+			// MONTHCAL_CLASS/SysMonthCal32: 1. Standard 178, 156 + WS_BORDER; 2. Luna 230, 170;
+			hMonthCal = CreateWindowW(L"SysMonthCal32", NULL,
+				WS_VISIBLE | WS_CHILD,
+				20, 200, 225, 160, hwnd, (HMENU) 4001, NULL, NULL);
+
+			// GroupBox
+			groupbox1 = CreateWindowW(TEXT("BUTTON"), TEXT("Choose Color"),
+				WS_CHILD | WS_VISIBLE | BS_GROUPBOX,
+				260, 20, 120, 150, hwnd, (HMENU) NULL, NULL, NULL);
+			radiobtn1 = CreateWindowW(TEXT("BUTTON"), TEXT("Blue"),
+				WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON | WS_GROUP,	// WS_GROUP: start exclusive radiobtn
+				270, 40, 100, 30, hwnd, (HMENU) 6001, NULL, NULL);
+			radiobtn2 = CreateWindowW(TEXT("BUTTON"), TEXT("Yellow"),
+				WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON | WS_GROUP,	// WS_GROUP: start another exclusive, until radiobtn3
+				270, 65, 100, 30, hwnd, (HMENU) 6002, NULL, NULL);
+			radiobtn3 = CreateWindowW(TEXT("BUTTON"), TEXT("Orange"),
+				WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON,
+				270, 90, 100, 30, hwnd, (HMENU) 6003, NULL, NULL);
+
+			SendMessage(radiobtn2, BM_SETCHECK, BST_CHECKED, TRUE);		// set default value to radiobtn2
+
+			// ComboBox: CBS_DROPDOWN or CBS_DROPDOWNLIST
+			hCombo = CreateWindowW(TEXT("COMBOBOX"), NULL,
+				WS_CHILD | WS_VISIBLE | CBS_HASSTRINGS | CBS_DROPDOWNLIST,
+				410, 20, 120, 110, hwnd, NULL, NULL, NULL);
+			SendMessageW(hCombo, CB_ADDSTRING, 0, (LPARAM) L"Windows 98 SE");
+			SendMessageW(hCombo, CB_ADDSTRING, 0, (LPARAM) L"Windows ME");
+			SendMessageW(hCombo, CB_ADDSTRING, 0, (LPARAM) L"Windows XP");
+			SendMessageW(hCombo, CB_ADDSTRING, 0, (LPARAM) L"Windows 7");
+			SendMessageW(hCombo, CB_ADDSTRING, 0, (LPARAM) L"Windows Phone 8");
+
+			SendMessageW(hCombo, CB_SETCURSEL, 3, 0);	// set default index for ComboBox
+			
+			// set font
+			SendMessageW(hCombo, WM_SETFONT, (WPARAM) hfont2, TRUE);
+			SendMessageW(groupbox1, WM_SETFONT, (WPARAM) hfont2, TRUE);
+			SendMessageW(radiobtn1, WM_SETFONT, (WPARAM) hfont2, TRUE);
+			SendMessageW(radiobtn2, WM_SETFONT, (WPARAM) hfont2, TRUE);
+			SendMessageW(radiobtn3, WM_SETFONT, (WPARAM) hfont2, TRUE);
 			break;
 
 		case WM_COMMAND:
@@ -374,7 +436,7 @@ LRESULT CALLBACK DialogProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) 
 					Beep(40, 50);
 					break;
 				case 650:
-					DestroyWindow(hwnd);
+					SendMessageW(hwnd, WM_CLOSE, 0, 0);
 					break;
 				case 700:
 					checked = IsDlgButtonChecked(hwnd, 700);
@@ -392,25 +454,42 @@ LRESULT CALLBACK DialogProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) 
 			}
 
 			if (HIWORD(wParam) == BN_CLICKED) {
-			   switch (LOWORD(wParam)) {
-				case ID_BLUE:
-					wcscpy(buf, L"blue");
-					SetWindowTextW(hDebugLabel, buf);
-					break;
-				case ID_YELLOW:
-					wcscpy(buf, L"yellow");
-					SetWindowTextW(hDebugLabel, buf);
-					break;
-				case ID_ORANGE:
-					wcscpy(buf, L"orange");
-					SetWindowTextW(hDebugLabel, buf);
-					break;
+				switch (LOWORD(wParam)) {
+					case 6001:
+						wcscpy(buf, L"blue");
+						SetWindowTextW(hDebugLabel, buf);
+						break;
+					case 6002:
+						wcscpy(buf, L"yellow");
+						SetWindowTextW(hDebugLabel, buf);
+						break;
+					case 6003:
+						wcscpy(buf, L"orange");
+						SetWindowTextW(hDebugLabel, buf);
+						break;
+				}
 			}
-			   
+
+			if (HIWORD(wParam) == CBN_SELCHANGE) {
+				sel = SendMessageW(hCombo, CB_GETCURSEL, 0, 0);
+				SendMessageW(hCombo, CB_GETLBTEXT, sel, (LPARAM) buf);
+				
+				SetWindowTextW(hDebugLabel, buf);			// fetch from ComboBox Control (much better)
+				//SetWindowTextW(hDebugLabel, os_list[sel]);	// from local buffer
+				
+				SetFocus(hwnd);
 			}
+
+			break;
+		case WM_ACTIVATE:
+			if (0 == wParam)             // becoming inactive
+               hDlgCurrent = NULL;
+            else                         // becoming active
+               hDlgCurrent = hwnd;
+			break;
+		case WM_SETFOCUS:
 			break;
 		case WM_SETFONT:
-			//MessageBoxW(NULL, L"Font set", L"First", MB_OK);
 			break;
 		case WM_HSCROLL:
 			ctrlID = GetDlgCtrlID((HWND) lParam);
@@ -425,15 +504,76 @@ LRESULT CALLBACK DialogProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) 
 			break;
 		case WM_NOTIFY:
 			lpNmHdr = (LPNMHDR) lParam;
-
-			if (lpNmHdr->code==MCN_SELECT) {
-				//ZeroMemory(&time, sizeof(SYSTEMTIME));
+			if (lpNmHdr->code == MCN_SELECT) {
+				ZeroMemory(&time, sizeof(SYSTEMTIME));
 				SendMessageW(hMonthCal, MCM_GETCURSEL, 0, (LPARAM) &time);
-  
 				swprintf(buf, 400, L"%d-%d-%d", time.wYear, time.wMonth, time.wDay);
 				SetWindowTextW(hDebugLabel, buf);
+			}
+			break;
+		case WM_CLOSE:
+			hwnd_parent = GetWindow(hwnd, GW_OWNER);
+			ret = EnableWindow(hwnd_parent, TRUE);
+			
+			if (ret == FALSE) MessageBoxW(NULL, L"GetWindow() FAIL", L"First", MB_OK);
+			DestroyWindow(hwnd);
+			break; 
+		case WM_DESTROY:
+			break;
+	}
+	
+	
+	return DefWindowProcW(hwnd, msg, wParam, lParam);
+	
+}
+
+
+LRESULT CALLBACK DialogProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	UINT checked;
+	int ctrlID;
+	int requestID;
+	int position;
+	LPNMHDR lpNmHdr;
+	SYSTEMTIME time;
+	int sel;
+
+	switch (msg) {
+		case WM_CREATE:
+			// default Height: Edit 21, Button 25, Static 13
+			hLabel = CreateWindowW(L"STATIC", L"This is Label", 
+				WS_CHILD | WS_VISIBLE | SS_LEFT, 
+				20, 20, 150, 13, hwnd, (HMENU) 500, NULL, NULL);
+			break;
+
+		case WM_COMMAND:
+			switch (LOWORD(wParam)) {
+				
+			}
+
+			if (HIWORD(wParam) == BN_CLICKED) {
+				switch (LOWORD(wParam)) {
+					
+				}
+			}
+
+			if (HIWORD(wParam) == CBN_SELCHANGE) {
 
 			}
+
+			break;
+		case WM_SETFONT:
+			//MessageBoxW(NULL, L"Font set", L"First", MB_OK);
+			break;
+		case WM_HSCROLL:
+			ctrlID = GetDlgCtrlID((HWND) lParam);
+			position = SendMessageW((HWND) lParam, TBM_GETPOS, 0, 0);
+			requestID = LOWORD(wParam);
+			switch (ctrlID) {
+
+			}
+			
+			break;
+		case WM_NOTIFY:
 			break;
 		case WM_CLOSE:
 			DestroyWindow(hwnd);
@@ -443,25 +583,8 @@ LRESULT CALLBACK DialogProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) 
 	}
 	
 	
-	return (DefWindowProc(hwnd, msg, wParam, lParam));
+	return DefWindowProcW(hwnd, msg, wParam, lParam);
 	
-}
-
-void RegisterDialogClass (HWND hwnd) {
-	WNDCLASSEX wc = {0};
-	
-	wc.cbSize = sizeof(WNDCLASSEX);
-	wc.lpfnWndProc= (WNDPROC) DialogProc;
-	wc.hInstance= ghInstance;
-	wc.hbrBackground= GetSysColorBrush(COLOR_3DFACE);
-	wc.lpszClassName= L"DialogClass";
-	RegisterClassEx(&wc);
-}
-
-void CreateDialogBox (HWND hwnd) {
-	CreateWindowEx(WS_EX_DLGMODALFRAME | WS_EX_TOPMOST,TEXT("DialogClass"), TEXT("Dialog Box"), 
-	WS_VISIBLE | WS_SYSMENU | WS_CAPTION , 100, 100, 200, 150, 
-	NULL, NULL, ghInstance,NULL);
 }
 
 COLORREF ShowColorDialog (HWND hwnd) {
@@ -544,7 +667,7 @@ void LoadFile_internal (LPCWSTR file) {
 	DWORD dwSize;
 	DWORD dw;
 	LPBYTE lpBuffer;
-	WCHAR ws_buf[50000] = {0};		/* BUG: buffer overflow ! */
+	WCHAR ws_buf[5000] = {0};		/* BUG: buffer overflow ! */
 
 	/* string consisting of several Asian characters */
 	wchar_t wcsString[] = L"\u9580\u961c\u9640\u963f\u963b\u9644";
