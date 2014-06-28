a385f0d2d395c09e353b93de08acf3559f7ab16a
Daniel Sirait <dsirait@aol.com>
2014-06-28 22:25:33 +0700

    M: msxml-sample.c, load from file

--- msxml-sample.c
+++ msxml-sample.c
@@ -1,7 +1,6 @@
 #define COBJMACROS
 
 
-
 #include <stdio.h>
 #include <windows.h>
 #include <msxml6.h>
@@ -11,15 +10,28 @@
 int main(){
 	IXMLDOMDocument *doc;
 	VARIANT_BOOL loaded;
-	BSTR xml = NULL;
+	BSTR xml;
+	BSTR bstr;
+	VARIANT varFileName;
+	VARIANT_BOOL isSuccessful;
 
+	VariantInit(&varFileName);
+	bstr = SysAllocString(L"myData.xml");
+	varFileName.vt = VT_BSTR;
+	varFileName.bstrVal = bstr;
+
+	
 	CoInitialize(NULL);
 	CoCreateInstance(&CLSID_DOMDocument60, NULL, CLSCTX_INPROC_SERVER, &IID_IXMLDOMDocument, (void**) &doc);
-	IXMLDOMDocument_loadXML(doc, L"<xml>aaabbb</xml>", &loaded);
+	
+
+	//IXMLDOMDocument_loadXML(doc, L"<root>aaabbb</root>", &loaded);	// load from string
+	IXMLDOMDocument_load(doc, varFileName, &isSuccessful);				// load from file
+
 	IXMLDOMDocument_get_xml(doc, &xml);
-
-
 	wprintf(L"XML = %s\n", xml);
+
+
 
 	SysFreeString(xml);
 	IXMLDOMDocument_Release(doc);
