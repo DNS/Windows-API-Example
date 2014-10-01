7a3106f97e8ca878db3eb12392f2e352793346f7
Daniel Sirait <dsirait@aol.com>
2014-10-02 06:56:46 +0700

    sync

--- simplewindow.c
+++ simplewindow.c
@@ -5,6 +5,14 @@
 */
 
 
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+	name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+	processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#pragma comment(lib, "Comctl32.lib")
+
+
+
 /* force MSVC to use WideChar function, must be before #include <windows.h> */
 #define UNICODE
 
@@ -15,12 +23,6 @@
 //#include <richedit.h>
 //#include <exdisp.h>
 
-
-#pragma comment(linker,"\"/manifestdependency:type='win32' \
-	name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
-	processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-
-#pragma comment(lib, "Comctl32.lib")
 
 
 #define IDM_FILE_NEW 11
